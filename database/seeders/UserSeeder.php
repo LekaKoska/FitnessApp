d9ed7985d2a8cedd837db49cae50cb09a98a6c42
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-03-19 10:43:10 +0100

    * Making Seeder and faker for users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Factory::create();
+
+        for($i = 0; $i < 50; $i++)
+        {
+            User::create(
+                [
+                    'name' => $faker->name,
+                    'email' => $faker->email,
+                    'password' => Hash::make("1234"),
+                ]);
+        }
     }
 }
