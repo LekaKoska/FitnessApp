e704775ac855a85c0bd44f8a527cae85629e00cf
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-03-19 11:38:14 +0100

    * Making View layout and navigation * Adding Templating sistem

---
+++ resources/views/navigation.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title></title>
+</head>
+<body>
+
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Shop</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Videos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+    </ul>
+
+
+</body>
+</html>
